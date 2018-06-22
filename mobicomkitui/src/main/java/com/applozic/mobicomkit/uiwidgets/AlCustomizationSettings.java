32edf6acda69a034e9c26b04de3a118967042694
Wilco Muit <wilco.muit@serviceheroes.com>
2018-06-22 14:00:00 +0200

    Added missing methods

--- mobicomkitui/src/main/java/com/applozic/mobicomkit/uiwidgets/AlCustomizationSettings.java
+++ mobicomkitui/src/main/java/com/applozic/mobicomkit/uiwidgets/AlCustomizationSettings.java
@@ -114,6 +114,15 @@ public class AlCustomizationSettings extends JsonMarker {
 
     private Map<String, Boolean> attachmentOptions;
 
+
+    public static boolean getCustomContactsList() {
+        return customContactsList;
+    }
+
+    public static void setCustomContactsList(boolean customContactsList) {
+        AlCustomizationSettings.customContactsList = customContactsList;
+    }
+
     public boolean isBroadcastOption() {
         return broadcastOption;
     }
