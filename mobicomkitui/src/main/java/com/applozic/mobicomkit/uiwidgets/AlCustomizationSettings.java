7e4cef99769a03438231be125c9589a1adf0e111
Ying Kun Zhan <yingkun.zhan@serviceheroes.com>
2018-06-25 10:21:00 +0200

    remove unused variable

--- mobicomkitui/src/main/java/com/applozic/mobicomkit/uiwidgets/AlCustomizationSettings.java
+++ mobicomkitui/src/main/java/com/applozic/mobicomkit/uiwidgets/AlCustomizationSettings.java
@@ -108,20 +108,11 @@ public class AlCustomizationSettings extends JsonMarker {
     private boolean enableMessageFastScroll = false;
     private static boolean addContactBroadcast = false;
     private static boolean createGroupBroadcast = false;
-    public static boolean customContactsList = false;
 
     private boolean launchChatFromProfilePicOrName;
 
     private Map<String, Boolean> attachmentOptions;
 
-
-    public static boolean getCustomContactsList() {
-        return customContactsList;
-    }
-
-    public static void setCustomContactsList(boolean customContactsList) {
-        AlCustomizationSettings.customContactsList = customContactsList;
-    }
 
     public boolean isBroadcastOption() {
         return broadcastOption;
