407beb7e68d7f404d0f7b8168ab8cbb1814d45dc
Ying Kun Zhan <yingkun.zhan@serviceheroes.com>
2018-07-13 14:49:47 +0200

    Added dutch channelMetadata

--- mobicomkitui/src/main/java/com/applozic/mobicomkit/uiwidgets/async/AlChannelCreateAsyncTask.java
+++ mobicomkitui/src/main/java/com/applozic/mobicomkit/uiwidgets/async/AlChannelCreateAsyncTask.java
@@ -7,6 +7,7 @@ import com.applozic.mobicomkit.api.people.ChannelInfo;
 import com.applozic.mobicomkit.channel.service.ChannelService;
 import com.applozic.mobicomkit.feed.ChannelFeedApiResponse;
 import com.applozic.mobicommons.people.channel.Channel;
+import com.applozic.mobicommons.people.channel.ChannelMetadata;
 
 /**
  * Created by Sunil on 12/26/2016.
@@ -21,6 +22,9 @@ public class AlChannelCreateAsyncTask extends AsyncTask<Void, Void, ChannelFeedA
     public AlChannelCreateAsyncTask(Context context, ChannelInfo channelInfo, TaskListenerInterface taskListenerInterface) {
         this.context = context;
         this.taskListenerInterface = taskListenerInterface;
+
+        setChannelMetaData(channelInfo);
+
         this.channelInfo = channelInfo;
         this.channelService = ChannelService.getInstance(context);
     }
@@ -51,5 +55,19 @@ public class AlChannelCreateAsyncTask extends AsyncTask<Void, Void, ChannelFeedA
         void onSuccess(Channel channel, Context context);
 
         void onFailure(ChannelFeedApiResponse channelFeedApiResponse, Context context);
+    }
+
+    private void setChannelMetaData(ChannelInfo channelInfo){
+        ChannelMetadata channelMetadata = new ChannelMetadata();
+        channelMetadata.setCreateGroupMessage(ChannelMetadata.ADMIN_NAME + " heeft de groep " + ChannelMetadata.GROUP_NAME + " aangemaakt.");
+        channelMetadata.setAddMemberMessage(ChannelMetadata.ADMIN_NAME + " heeft " + ChannelMetadata.USER_NAME + " toegevoegd.");
+        channelMetadata.setRemoveMemberMessage(ChannelMetadata.ADMIN_NAME + " heeft " + ChannelMetadata.USER_NAME + " verwijderd.");
+        channelMetadata.setGroupNameChangeMessage(ChannelMetadata.USER_NAME + " heeft de groepsnaam veranderd naar " + ChannelMetadata.GROUP_NAME);
+        channelMetadata.setJoinMemberMessage(ChannelMetadata.USER_NAME + " is toegetreden tot de groep");
+        channelMetadata.setGroupLeftMessage(ChannelMetadata.USER_NAME + " heeft de groep " + ChannelMetadata.GROUP_NAME + " verlaten.");
+        channelMetadata.setGroupIconChangeMessage(ChannelMetadata.USER_NAME + " heeft de groepsafbeelding aangepast.");
+        channelMetadata.setDeletedGroupMessage(ChannelMetadata.ADMIN_NAME + " heeft de groep " + ChannelMetadata.GROUP_NAME + " verwijderd.");
+
+        channelInfo.setChannelMetadata(channelMetadata);
     }
 }
